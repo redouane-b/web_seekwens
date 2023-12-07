23bd95c66bbb7b91b9e1788f73f04521c58920e0
Redouane @vivobook <bouazza.re@gmail.com>
2023-12-07 22:02:06 +0100

    good nav comportment and background image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-// script.js
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+const navBar = document.getElementById("mainNav");
+window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 100) {
+            navBar.style.top = "0";
+    } else {
+        navBar.style.top = `-${navBar.clientHeight}px`;
+    }
+};
+
+
+
 
 // Example: Log a message to the console when the page loads
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Page loaded!');
 });
+
+
