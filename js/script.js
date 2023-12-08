733872d42677f6836b3c2e98132b998696824049
Redouane @vivobook <bouazza.re@gmail.com>
2023-12-08 02:47:36 +0100

    version 0 with all componenets

--- js/script.js
+++ js/script.js
@@ -14,12 +14,30 @@ const navBar = document.getElementById("mainNav");
 window.onscroll = function () {
     const currentScrollPos = window.pageYOffset;
 
-    if (currentScrollPos > 100) {
+    if (currentScrollPos > 500) {
             navBar.style.top = "0";
     } else {
         navBar.style.top = `-${navBar.clientHeight}px`;
     }
 };
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const definitionSection = document.querySelector('.definition-section');
+    let initialBackgroundPosition = 500;
+
+    console.log(initialBackgroundPosition)
+    window.addEventListener('scroll', function () {
+        const scrollPosition = window.scrollY;
+        const newBackgroundPosition = initialBackgroundPosition + scrollPosition * 0.5;
+        console.log(newBackgroundPosition)
+
+
+        definitionSection.style.backgroundPositionY = `${newBackgroundPosition}px`;
+    });
+});
+
+
 
 
 
